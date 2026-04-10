bd3631ba744ba423df3b8bfbdac4f40a95615c16
DarkFallenStar <kennykaven@gmail.com>
2026-04-10 07:46:37 -0500

    a

--- src/app.js
+++ src/app.js
@@ -8,10 +8,18 @@ app.get('/hola', (req, res) => {
  res.json({ mensaje: '¡Hola Mundo desde Express!' });
 });
 
-app.get('/metros?num', (req, res) => {
+app.get('/metros/:num', (req, res) => {
     const num = req.params.num
     const pies = num * 3.28
     res.json({ mensaje:`'¡Hola, ${num} metros son ${pies} pies!` });
+});
+
+app.get("/contra/:password", (req, res) => {
+    const password = req.params.password;
+    if (password.length < 8 || !/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/[0-9]/.test(password)) {
+        res.json({ mensaje: "La contraseña no es segura" });
+    } else {        res.json({ mensaje: "La contraseña es segura" });
+    }
 });
 
 
